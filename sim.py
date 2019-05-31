d8186e9971c9d052b20626f49fe2e2aae9f87667
bwang0202@gmail.com <bojun@privatemachines.com>
2019-05-30 18:35:44 -0700

    all rules added

--- sim.py
+++ sim.py
@@ -3,6 +3,11 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# CONFIG PARAM
+OPTIONAL1 = False
+OPTIONAL2 = False
+
 # Simpy Params
 UNIT_TIME = 1
 WK = 1
@@ -30,6 +35,7 @@ X = 0.1
 Y = 0.8
 T = 0.3
 V = 0.3
+W = 0.1
 
 assert(P + Q <= 1)
 assert(X + Y <= 1)
@@ -47,7 +53,27 @@ class Node:
     def evolve(self, env, network):
         while True:
             yield env.timeout(UNIT_TIME)
+            prob = random.random()
             # Rules of node changing goes here
+            if self.type == MN:
+                if prob < R:
+                    self.type = BM
+            elif self.type == BW:
+                if prob < P:
+                    self.type = SW
+                elif prob < P + Q:
+                    self.type = FW
+            elif self.type == SW:
+                if prob < X:
+                    self.type = MN
+                elif prob < X + Y:
+                    self.type = WK
+            elif self.type == FW:
+                if prob < N:
+                    self.type = WK
+            elif self.type == BM:
+                if prob < T:
+                    self.type = WK
 
 
 class Edge:
@@ -59,6 +85,35 @@ class Edge:
         while True:
             yield env.timeout(UNIT_TIME)
             # Rules of edge changing goes here
+            prob = random.random()
+            a_type = network.get_node_type(self.idx_a)
+            b_type = network.get_node_type(self.idx_b)
+            # AW - MB
+            if (a_type == BM and b_type == WK) or (b_type == BM and a_type == WK):
+                if prob < M:
+                    if a_type == BM:
+                        network.set_node_type(self.idx_a, MN)
+                        network.set_node_type(self.idx_b, BW)
+                    else:
+                        network.set_node_type(self.idx_b, MN)
+                        network.set_node_type(self.idx_a, BW)
+            if OPTIONAL1:
+                if (a_type == MN and b_type == SW) or (b_type == MN and a_type == SW):
+                    if prob < V:
+                        if a_type == MN:
+                            network.set_node_type(self.idx_b, MN)
+                        else:
+                            network.set_node_type(self.idx_a, MN)
+            if OPTIONAL2:
+                if (a_type == BM and b_type == SW) or (b_type == BM and a_type == SW):
+                    if prob < W:
+                        if a_type == BM:
+                            network.set_node_type(self.idx_a, MN)
+                            network.set_node_type(self.idx_b, BW)
+                        else:
+                            network.set_node_type(self.idx_b, MN)
+                            network.set_node_type(self.idx_a, BW)
+
 
 
 class Network:
